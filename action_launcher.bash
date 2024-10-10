c1ba165f5cfe27824fdf57b86b1014b8bf2209b1
Henner Zeller <hzeller@google.com>
2024-10-10 15:01:06 -0700

    Playing with directory.

--- action_launcher.bash
+++ action_launcher.bash
@@ -16,7 +16,7 @@ fi
 repository_name="$(basename "$GITHUB_REPOSITORY")"
 recreated_runner_dir="$INPUT_REPO_PATH_PREFIX/$repository_name"
 mkdir -p "$recreated_runner_dir"
-recreated_repo_dir="$recreated_runner_dir/$repository_name"
+recreated_repo_dir="$recreated_runner_dir/${repository_name}-recreated"
 
 ln -s "$(pwd)" "$recreated_repo_dir"
 
